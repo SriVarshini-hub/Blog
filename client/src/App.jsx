0b7ac4ec179c44a6999ea3daa0bd2728f7bbd630
SriVarshiniSS <srivarshinivivekanandan14@gmail.com>
2025-04-25 18:11:56 +0530

    Blog

---
+++ client/src/App.jsx
@@ -0,0 +1,63 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Write from "./Pages/Write";
+import Home from "./Pages/Home";
+import Single from "./Pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./style.scss";
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+}
+export default App;
